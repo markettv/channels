d0766667fd8c62f45e81f6f6516c28faf0c50f91
markettv <168855912+markettv@users.noreply.github.com>
2024-05-27 01:57:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-install
+# 1. الاتصال بالجهاز عبر SSH
+ssh root@<IP_ADDRESS_OF_VUPLUS>
+
+# 2. التنقل إلى مجلد /tmp وتنزيل الملف
+cd /tmp
+wget https://github.com/markettv/channels/raw/main/bouquets24_backup.tar
+
+# 3. فك ضغط الملف
+tar -xvf bouquets24_backup.tar
+
+# 4. نسخ الملفات إلى المسار /etc/enigma2
+cp -r /tmp/* /etc/enigma2/
+
+# 5. إعادة تشغيل الجهاز
+reboot
+
